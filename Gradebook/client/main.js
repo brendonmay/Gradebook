2ec18bf7b3d2965f611bb8549ed8bc6c0a6e4c1a
unknown <brendonscottmay@gmail.com>
2018-02-06 15:14:36 -0500

    added footer, added year form for modal, courses now appear in sidebar when added, can now console log the year input from modal.

--- Gradebook/client/main.js
+++ Gradebook/client/main.js
@@ -1,29 +1,53 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import { Courses } from '../lib/collections.js';
+import { Accounts } from 'meteor/accounts-base';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+// Template.hello.onCreated(function helloOnCreated() {
+//   // counter starts at 0
+//   this.counter = new ReactiveVar(0);
+// });
 
+Template.sideNav.helpers({
+  // courses:[
+  //   {course: "Math"},
+  //   {course: "Science"},
+  //   {course: "History"}
+  // ],
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
+  courses(){
+    return Courses.find({});
   },
+
 });
 
+Template.addCourse.events({
+  //type of event is a submit, the element is a form with class add-form, when its called run a function
+  'submit .add-form': function(){
+    //prevent from being submitted into another file
+    event.preventDefault();
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+    //Get input value
+    const target = event.target;
+    const course = target.courseName.value;
+    const year = target.courseYear.value;
+    console.log(year);
 
+    //insert course and year into collection Courses
+    Courses.insert({
+      course,
+      year,
+      owner: Meteor.userId(),
+    });
 
-$(document).ready(function(){
-  $('.modal').modal();
+    //Clear form
+    target.courseName.value="";
+    target.courseYear.value="";
+
+    //Close Modal
+    $('#addModal').modal('close');
+  }
+
 });

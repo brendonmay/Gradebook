33b122dcd631e2a4c00c9b3b799878c937a02881
Jason <jason.haddad7@gmail.com>
2018-02-07 18:14:47 -0500

    update

--- Gradebook/client/main.js
+++ Gradebook/client/main.js
@@ -1,28 +1,61 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import { Courses } from '../lib/collections.js';
+import { Accounts } from 'meteor/accounts-base';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+// Template.hello.onCreated(function helloOnCreated() {
+//   // counter starts at 0
+//   this.counter = new ReactiveVar(0);
+// });
+
+Template.addCourse.events({
+  //type of event is a submit, the element is a form with class add-form, when its called run a function
+  'submit .add-form': function(){
+    //prevent from being submitted into another file
+    event.preventDefault();
+
+    //Get input value
+    const target = event.target;
+    const course = target.courseName.value;
+    const year = target.courseYear.value;
+    console.log(year);
+
+    //insert course and year into collection Courses
+    Courses.insert({
+      course,
+      year,
+      owner: Meteor.userId(),
+    });
+
+    //Clear form
+    target.courseName.value="";
+    target.courseYear.value="";
+
+    //Close Modal
+    $('#addModal').modal('close');
+  }
+
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
+Template.sideNavDropDown.onRendered(function() {
+  this.$('.collapsible').collapsible();
+});
+
+Template.sideNavDropDown.helpers({
+  // courses:[
+  //   {course: "Math"},
+  //   {course: "Science"},
+  //   {course: "History"}
+  // ],
+
+  courses(){
+    return Courses.find({});
   },
+
 });
 
-
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
-
-$(document).ready(function(){
-  $('.modal').modal();
+Template.tabsContent.onRendered(function() {
+  this.$('.tabs').tabs();
 });

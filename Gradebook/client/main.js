31e70f8b4087ce11fc8eb130177f48817cdbbab7
joshuacurrie <j3currie@uwaterloo.ca>
2018-02-09 19:22:44 -0500

    Updated course tabs template with correct functionality. Also, added courseIdDisplay as a Session Var instead of courseName

--- Gradebook/client/main.js
+++ Gradebook/client/main.js
@@ -5,9 +5,6 @@ import { Accounts } from 'meteor/accounts-base';
 
 import './main.html';
 
-// Template.addCourse.onCreated(function addCourseOnCreated() {
-//  
-// });
 
 Template.addCourse.events({
   //type of event is a submit, the element is a form with class add-form, when its called run a function
@@ -22,17 +19,10 @@ Template.addCourse.events({
     const target = event.target;
     const course = target.courseName.value;
     const year = target.courseYear.value;
-    //console.log(year);
 
     //check if user has ever created a course
     //if user has not created a course,
     if (Courses.findOne({ownerId: Meteor.userId()}) == null) {
-      // Courses.insert({
-      //   ownerId: Meteor.userId(),
-      //   courses:[
-      //     {courseId: 1, courseName: course, courseYear: year}
-      //   ]
-      // });
       Meteor.call('courses.createFirstCourse', course, year);  
     } 
     else {
@@ -44,30 +34,19 @@ Template.addCourse.events({
           const docLength = doc.courses.length;
           const lastCourseId = doc.courses[docLength-1].courseId;
           newCourseId = lastCourseId + 1;
-          //console.log("new course id: " + newCourseId)
         });
       
       //insert new course into collection
 
       //determine courses they currently have
       let currentCourses = Courses.findOne({ownerId: Meteor.userId()}, {_id: 0, ownerId: 0}).courses; //array of course objects
-      //console.log("current courses: " + currentCourses);
 
       //create a new course object to be inserted
       const newCourse = {courseId: newCourseId, courseName: course, courseYear: year};
-      //console.log("new course: " + newCourse);
 
       //create updated array of course objects
       currentCourses[newCourseId - 1] = newCourse;
-      //console.log("updated courses" + currentCourses);
 
-      //update document in collection to include the new course
-      // Courses.update( //won't work until you change this to a method since its considered insecure, to make it temp. work, query {"_id": "SqfkSQWNpEmjosBia"}
-      //   {"ownerId": Meteor.userId()},
-      //   {$set:
-      //     {"courses": currentCourses}
-      //   }
-      // );
       Meteor.call('courses.updateCourses', currentCourses);
     }
 
@@ -93,15 +72,11 @@ Template.sideNavDropDown.onRendered(function() {
 });
 
 Template.sideNavDropDown.helpers({
-  // idGen: function(){
-  //   return [{id:1}, {id:2}, {id:3}];
-  // },
 
   courses: function(year){
     //need to put all courses with the courseYear == year into object and return that
     let coursesWithSameYear = [];
 
-    //console.log("year: " + year);
     const teacherInfo = Courses.find({ownerId: Meteor.userId()}, {_id: 0, ownerId: 0});
     teacherInfo.forEach(
       function(doc) {
@@ -114,7 +89,6 @@ Template.sideNavDropDown.helpers({
           }
         }
       });
-      //console.log("coursesWithSameYear: " + coursesWithSameYear);
     return coursesWithSameYear;
   },
 
@@ -139,7 +113,6 @@ Template.sideNavDropDown.helpers({
           }
         }
       });
-      //console.log("uniqueYears: " + uniqueYears);
     return uniqueYears;
   },
 
@@ -159,24 +132,18 @@ Template.sideNavDropDown.events({
     event.preventDefault();
 
     const target = event.target;
-    var testName = document.getElementById(target.id).innerText;
+    var courseId = target.id;
 
-    Session.set('courseNameDisplay', testName);
-    var testing = Session.get('courseNameDisplay');
+    Session.set('courseIdDisplay', courseId);
+    //Use Session.get('courseIdDisplay'); to grab the courseId from sessions
 
-    console.log(testing);
   }
 });
 
 Template.testContent.helpers({
   display: function(){
-    return Session.get('courseNameDisplay');
+    return Session.get('courseIdDisplay');
   }
 });
 
-/*Template.testContent.helpers({
-  courses(){
-    return Courses.find({});
-  },
-});*/
 

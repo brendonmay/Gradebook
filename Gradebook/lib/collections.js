e2756c211809764a6b8a4408e53d58f31376f911
unknown <brendonscottmay@gmail.com>
2018-02-12 18:32:05 -0500

    Created new Collection, CourseInformation and a method to add default settings to the collection

--- Gradebook/lib/collections.js
+++ Gradebook/lib/collections.js
@@ -3,7 +3,8 @@ import { Meteor } from "meteor/meteor";
 import { check } from 'meteor/check';
 
 //don't forget to remove insecure and then add methods --> meteor remove insecure
-export const Courses = new Mongo.Collection("courses")
+export const Courses = new Mongo.Collection("courses");
+export const CourseInformation = new Mongo.Collection("courseInformation");
 
 //define our methods
 
@@ -23,5 +24,23 @@ Meteor.methods({
                 {"courses": currentCourses}
             }
         );
+    },
+    'courseInformation.defaultSettings'(courseId){
+        CourseInformation.insert({
+            ownerId: Meteor.userId(),
+            courseId: courseId,
+            categoryWeighting: {K: 25, A:25, T:25, C:25},
+            courseworkWeight: 70,
+            finalWeighting: 30,
+            courseworkAssessmentTypes:[
+                {assessmentType: "Quiz", assessmentWeight: 10},
+		        {assessmentType: "Assignment", assessmentWeight: 25},
+		        {assessmentType: "Test", assessmentWeight: 35}
+            ],
+            finalAssessmentTypes:[
+                {assessmentType: "Culminating Task", assessmentWeight: 15},
+		        {assessmentType: "Final Exam", assessmentWeight: 15}
+            ]
+        });
     },
 });

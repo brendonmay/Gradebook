ad6bfc3b3053c173f6e94297ac3f66cb6ca65f55
unknown <brendonscottmay@gmail.com>
2018-02-19 22:08:54 -0500

    fixed bug with courseSettingsNav, now nothing will happen when you click left or right of it.

--- Gradebook/client/includes/courseSettingsNavBar/courseSettingsNavBar.js
+++ Gradebook/client/includes/courseSettingsNavBar/courseSettingsNavBar.js
@@ -24,8 +24,10 @@ Template.courseSettingsNavBar.events({
             settingScreen = "Assessments";
         } else if (settingId == "AW") {
             settingScreen = "Assessment Weightings";
-        } else if (settingID == "GS") {
+        } else if (settingId == "GS") {
             settingScreen = "General Settings"
+        }else{
+            settingScreen = Session.get('settingScreenText')
         }
 
         //console.log(settingScreen);

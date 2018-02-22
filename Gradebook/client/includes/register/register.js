41da226c5251ffb04ae977bb3bc6cfe5b4be795b
Adam Beckett May <adam.may@uoit.net>
2018-02-22 15:31:47 -0500

    made a publish and subbscribe for our users databse, and have a toast appearing when you try to register with an already existing email

--- Gradebook/client/includes/register/register.js
+++ Gradebook/client/includes/register/register.js
@@ -4,63 +4,69 @@ import { Accounts } from 'meteor/accounts-base';
 
 import '../../main.html';
 
-if(Meteor.isClient){
-    Template.register.events({
-        'click .cancel-button': function(){
-            //clear the input fields
-            document.getElementById("registerForm").reset();
 
-            //if cancel button is clicked, close the modal
-            $('#registerModal').modal('close');
-        },
+Meteor.subscribe("users");
 
-        'click .back-button': function(){
-            document.getElementById("registerForm").reset();
+Template.register.events({
+    'click .cancel-button': function(){
+        //clear the input fields
+        document.getElementById("registerForm").reset();
 
-            $('#registerModal').modal('close');
-            $('#loginModal').modal('open');
-        },
+        //if cancel button is clicked, close the modal
+        $('#registerModal').modal('close');
+    },
 
-        'submit .register-form': function(event, template) {
-            event.preventDefault();
+    'click .back-button': function(){
+        document.getElementById("registerForm").reset();
 
-            var emailVar = event.target.registerEmail.value;
-            console.log(emailVar);
-            var passwordVar = event.target.registerPassword.value;
+        $('#registerModal').modal('close');
+        $('#loginModal').modal('open');
+    },
 
-            /*I was not able to use a findOne() method. Since we are not logged into the site, we do not have a parameter to use when searching for emails. 
-            The find() would be more suitable since it would output all entries, and then we could itterate through that with our email condition.*/
-            // const userEmail = Meteor.users.findOne({_id : this._id}); //came up undefined
-            // console.log(userEmail);
+    'submit .register-form': function(event, template) {
+        event.preventDefault();
 
-            // const userList = Meteor.users.find();
-            // userList.forEach(
-            //      function (doc) {
-            //          for(var i = 0; i < doc.users.length; i++){ 
-            //                /*runs through the collection within the specified index (emails) - My error appears here. The result is the same 
-            //                whether i change email to users. I still think the issue has to do with server publication but i am not certain.*/
-                        
-            //              const userListEmail = doc.emails[i].address; //pulls the field to be compared
-            //              console.log(userListEmail); //outputs it to the console, a check to see that it is pulling the information correctly
-            //          }
-            //      });
-            //const emailCheck = userList.emails[0].address;
-            //console.log(emailCheck);
+        var emailVar = event.target.registerEmail.value;
+        console.log(emailVar);
+        var passwordVar = event.target.registerPassword.value;
 
-            // if(emailCheck == true){
-            //     Materialize.toast('User already exists', 3000, 'amber darken-3')
-            //     return false;
-            // }
-            // else{
-                Accounts.createUser({
-                    email: emailVar,
-                    password: passwordVar
-                });
-                document.getElementById("registerForm").reset();
-                $('#registerModal').modal('close');
-            // }
+        const emailCheck = Meteor.users.find({"emails.address" : emailVar}).fetch();
+        console.log(emailCheck[0]);
 
-            /*Try looking into the Accounts.findUserByEmail method... i think that is what is needed.*/
+        /*I was not able to use a findOne() method. Since we are not logged into the site, we do not have a parameter to use when searching for emails. 
+        The find() would be more suitable since it would output all entries, and then we could itterate through that with our email condition.*/
+        // const userEmail = Meteor.users.findOne({_id : this._id}); //came up undefined
+        // console.log(userEmail);
+
+        // const userList = Meteor.users.find();
+        // userList.forEach(
+        //      function (doc) {
+        //          for(var i = 0; i < doc.users.length; i++){ 
+        //                /*runs through the collection within the specified index (emails) - My error appears here. The result is the same 
+        //                whether i change email to users. I still think the issue has to do with server publication but i am not certain.*/
+                    
+        //              const userListEmail = doc.emails[i].address; //pulls the field to be compared
+        //              console.log(userListEmail); //outputs it to the console, a check to see that it is pulling the information correctly
+        //          }
+        //      });
+        //const emailCheck = userList.emails[0].address;
+        //console.log(emailCheck);
+
+        if(emailCheck != null){
+            Materialize.toast('User already exists', 3000, 'amber darken-3')
+            return false;
         }
-    })
-}
+        else{
+            Accounts.createUser({
+                email: emailVar,
+                password: passwordVar
+            });
+            document.getElementById("registerForm").reset();
+            $('#registerModal').modal('close');
+        }
+
+        /*Try looking into the Accounts.findUserByEmail method... i think that is what is needed.*/
+    }
+})
+
+
--- Gradebook/server/main.js
+++ Gradebook/server/main.js
@@ -4,3 +4,7 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+Meteor.publish("users", function(){
+  return Meteor.users.find();
+})

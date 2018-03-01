08b0ad71464f5549d2a01c5640ee3fcf28a48c0a
Joshua Currie <joshua@interaxon.ca>
2018-03-01 15:26:45 -0500

    initial changes for gradebook static row and column

--- Gradebook/client/includes/gradeBookChart/gradeBookChart.js
+++ Gradebook/client/includes/gradeBookChart/gradeBookChart.js
@@ -37,6 +37,61 @@ function canAssignFinalEvaluation() {
     return arrayOfEvaluationsToReturn.length != 0
 }
 
+function moveScroll() {
+    var scroll_top = $(window).scrollTop();
+    var scroll_left = $(window).scrollLeft();
+    var anchor_top = $("#main_table").offset().top;
+    var anchor_left = $("#main_table").offset().left;
+    var anchor_bottom = $("#bottom_anchor").offset().top;
+    
+    $("#clone").find("thead").css({
+        
+        position: 'absolute',
+        left: - scroll_left  + 'px'
+    });
+    
+    $("#main_table").find(".first").css({
+        position: 'absolute',
+        left: scroll_left + anchor_left + 'px'
+    });
+    
+    if (scroll_top >= anchor_top && scroll_top <= anchor_bottom) {
+        clone_table = $("#clone");
+        if (clone_table.length == 0) {
+            clone_table = $("#main_table")
+                .clone()
+                .attr('id', 'clone')
+                .attr('align', 'center')
+                .css({
+                    position: 'fixed',
+                    pointerEvents: 'none',
+                    left: $("#main_table").offset().left+'px',
+                    top: 0
+                })
+                .appendTo($("#table_container"))
+                .css({
+                    visibility: 'hidden'
+                })
+                .find("thead").css({
+                    visibility: 'visible'
+                });
+        }
+    }
+    else {
+        $("#clone").remove();
+    }
+}
+
+Template.gradeBookChart.onRendered(function() {
+    $("#main_table")
+    .wrap('<div id="table_container"></div>')
+    .after('<div id="bottom_anchor"></div>');
+    
+    $(window).scroll(moveScroll);
+});
+
+
+
 Template.gradeBookChart.events({
     'click .assignFinalEvalButtonGradeBook': function () {
         //check if you have no more evaluations to assign

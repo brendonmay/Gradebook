a26a41fb0937282339193611c7c8b16aacd25e69
Joshua Currie <joshua@interaxon.ca>
2018-03-08 15:59:39 -0500

    fixed register issues

--- Gradebook/client/includes/register/register.js
+++ Gradebook/client/includes/register/register.js
@@ -39,55 +39,18 @@ Template.register.events({
         $('#loginModal').modal('open');
     },
 
-    'submit .register-form': function (event, template) {
+    'submit .register-form': function (event) {
         event.preventDefault();
 
         var emailVar = event.target.registerEmail.value;
         var passwordVar = event.target.registerPassword.value;
-        var confirmPass = event.target.confirmPassword.value;
+        Accounts.createUser({
+            email: emailVar,
+            password: passwordVar
+        });
 
-        //regEx used for password verification
-        var lowercase = /[a-z]/g;
-        var uppercase = /[A-Z]/g;
-        var number = /[0-9]/g;
-
-        // const emailCheck = Meteor.users.find({ "emails.address": emailVar }).fetch();
-
-        //a count that will allow us to post multiple toasts to appear on the screen at once
-        var toastCount = 0;
-
-        if (emailCheck[0] != null) {
-            Materialize.toast('User already exists', 5000, 'amber darken-3')
-            toastCount++;
-        }
-        if (!(passwordVar.match(lowercase))) {
-            Materialize.toast('Password must contain a lowercase letter', 5000, 'amber darken-3')
-            toastCount++;
-        }
-        if (!(passwordVar.match(uppercase))) {
-            Materialize.toast('Password must contain an uppercase letter', 5000, 'amber darken-3')
-            toastCount++;
-        }
-        if (!(passwordVar.match(number))) {
-            Materialize.toast('Password must contain a number', 5000, 'amber darken-3')
-            toastCount++;
-        }
-        if (confirmPass != passwordVar) {
-            Materialize.toast('Your passwords do not match.', 5000, 'amber darken-3')
-            toastCount++;
-        }
-        if (toastCount > 0) {
-            document.getElementById("registerForm").reset();
-            //return false;
-        }
-        else {
-            Accounts.createUser({
-                email: emailVar,
-                password: passwordVar
-            });
-            document.getElementById("registerForm").reset();
-            $('#registerModal').modal('close');
-        }
+        document.getElementById("registerForm").reset();
+        $('#registerModal').modal('close');
     }
 })
 
@@ -113,7 +76,7 @@ Template.register.onRendered(function () {
     });
 
     $("#registerForm").validate({
-        errorClass: "validation-red-text",
+        errorClass: "invalid validation-red-text",
         validClass: "",
         rules: {
             registerEmail: {
@@ -143,8 +106,8 @@ Template.register.onRendered(function () {
             registerPassword: {
                 required: "You must enter a password.",
                 minlength: "Your password must be at least 7 characters.",
-                containsUppercase: "Your password must contain atleast 1 upper case letter.",
-                containsNumber: "Your password must contain atleast 1 number."
+                containsUppercase: "Your password must contain at least 1 upper case letter.",
+                containsNumber: "Your password must contain at least 1 number."
 
             },
             confirmPassword: {

45505b4cd0a66e19003a828d6e252ba0ecb7ce87
unknown <brendonscottmay@gmail.com>
2018-03-19 10:49:28 -0400

    fixed gradebookcolor update and set functions, also fixed heads on gb

--- Gradebook/client/includes/sideNavDropDown/sideNavDropDown.js
+++ Gradebook/client/includes/sideNavDropDown/sideNavDropDown.js
@@ -61,15 +61,19 @@ function updateGradebookColors() {
             for (z = 0; z < arrayofStudentIds.length; z++) {
                 if (category == "C") {
                     var studentId = arrayofStudentIds[z];
-                    document.getElementById(category + "?" + studentId + "#" + assessmentId).parentElement.style = "background-color: #9e9e9e; border-right: 2px solid black";
-                    document.getElementById(category + "?" + studentId + "#" + assessmentId).disabled = "true";
-                    document.getElementById(category + "?" + studentId + "#" + assessmentId).value = "N/A";
+                    if (studentId != "s-0"){
+                        document.getElementById(category + "?" + studentId + "#" + assessmentId).parentElement.style = "background-color: #9e9e9e; border-right: 2px solid black";
+                        document.getElementById(category + "?" + studentId + "#" + assessmentId).disabled = "true";
+                        document.getElementById(category + "?" + studentId + "#" + assessmentId).value = "N/A";
+                    }
                 }
                 else {
                     var studentId = arrayofStudentIds[z];
-                    document.getElementById(category + "?" + studentId + "#" + assessmentId).parentElement.style = "background-color: #9e9e9e";
-                    document.getElementById(category + "?" + studentId + "#" + assessmentId).disabled = "true";
-                    document.getElementById(category + "?" + studentId + "#" + assessmentId).value = "N/A";
+                    if (studentId != "s-0"){
+                        document.getElementById(category + "?" + studentId + "#" + assessmentId).parentElement.style = "background-color: #9e9e9e";
+                        document.getElementById(category + "?" + studentId + "#" + assessmentId).disabled = "true";
+                        document.getElementById(category + "?" + studentId + "#" + assessmentId).value = "N/A";
+                    }
                 }
             }
         }
@@ -78,12 +82,14 @@ function updateGradebookColors() {
 
             for (z = 0; z < arrayofStudentIds.length; z++) {
                 var studentId = arrayofStudentIds[z];
-                document.getElementById(category + "?" + studentId + "#" + assessmentId).parentElement.style = "";
-                document.getElementById(category + "?" + studentId + "#" + assessmentId).removeAttribute("disabled");
-                var oldValue = findOldStudentGradeValue(studentId, assessmentId, category, Meteor.userId(), courseId);
-                document.getElementById(category + "?" + studentId + "#" + assessmentId).value = oldValue;
-                if (category == "C") {
-                    document.getElementById(category + "?" + studentId + "#" + assessmentId).parentElement.style = "border-right: 2px solid black";
+                if (studentId != "s-0"){
+                    document.getElementById(category + "?" + studentId + "#" + assessmentId).parentElement.style = "";
+                    document.getElementById(category + "?" + studentId + "#" + assessmentId).removeAttribute("disabled");
+                    var oldValue = findOldStudentGradeValue(studentId, assessmentId, category, Meteor.userId(), courseId);
+                    document.getElementById(category + "?" + studentId + "#" + assessmentId).value = oldValue;
+                    if (category == "C") {
+                        document.getElementById(category + "?" + studentId + "#" + assessmentId).parentElement.style = "border-right: 2px solid black";
+                    }
                 }
             }
         }
@@ -210,7 +216,7 @@ Template.sideNavDropDown.events({
             var delayInMilliseconds = 125;
             setTimeout(function() {
                 updateGradebookColors();
-                //$("#main_table").tableHeadFixer({ "left": 1, 'head': true });
+                $("#main_table").tableHeadFixer({ "left": 1, 'head': true });
                 console.log("Gradebook colors updated.");
               }, delayInMilliseconds);
         }).catch(function(){

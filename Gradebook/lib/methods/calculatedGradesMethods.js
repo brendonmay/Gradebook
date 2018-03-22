5fd0322213f88f10a76bfc711cf2e5131e29a69e
unknown <brendonscottmay@gmail.com>
2018-03-21 23:37:27 -0400

    outlined all the methods we will need for calculatedgrades

--- Gradebook/lib/methods/calculatedGradesMethods.js
+++ Gradebook/lib/methods/calculatedGradesMethods.js
@@ -3,8 +3,42 @@ import { Meteor } from "meteor/meteor";
 
 if (Meteor.isServer) {
     Meteor.methods({
-        'calculatedgrades.defaultSetup'(){
+        'calculatedgrades.defaultSetup'(ownerId, courseId){
+            CalculatedGrades.insert({
+                ownerId,
+                courseId,
+                students: []
+            })
+        },
+        'calculatedgrades.deleteCourse'(){
 
         },
+        'calculatedgrades.addFirstStudent'(){
+
+        },
+        'calculatedgrades.addStudent'(){
+
+        },
+        'calculatedgrades.deleteStudent'(){
+
+        },
+        'calculatedgrades.addNewAssessmentType'(){
+
+        },
+        'calculatedgrades.deleteAssessmentType'(){
+
+        },
+        'calculatedgrades.addNewAssessment'(){
+
+        },
+        'calculatedgrades.deleteAssessment'(){
+
+        },
+        'calculatedgrades.updateAssessmentGrade'(assessmentId, KGrade, AGrade, TGrade, CGrade){
+
+        },
+        'calculatedgrades.updateAssessmentTypeGrade'(assessmentTypeId, KGrade, AGrade, TGrade, CGrade){
+
+        }
     });
 }

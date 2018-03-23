385b8bbf95d636082bbbe1c883377a9930519379
unknown <brendonscottmay@gmail.com>
2018-03-23 15:43:17 -0400

    styling is now done in studentReports

--- Gradebook/client/includes/studentReports/studentReports.js
+++ Gradebook/client/includes/studentReports/studentReports.js
@@ -98,24 +98,26 @@ function getCourseEvalName(assessmentId) {
 
 function drawAssessmentBreakdownBarGraph() {
     //clear the contents of the div, in the event this function is called more than once.
+    var data = grabGrades("c1"); //should be the assessmentTypeId
+    console.log(data)
     new Morris.Bar({
         // ID of the element in which to draw the chart.
         element: 'assessmentBreakdownBarGraph',
         // Chart data records -- each entry in this array corresponds to a point on
         // the chart.
-        // data: [
-        //     { assessmentName: 'Quiz 1', K: 100, A: 90, T: 80, C:50 },
-        //     { assessmentName: 'Quiz 2', K: 75,  A: 65, T: 80, C:50 },
-        //     { assessmentName: 'Quiz 3', K: 50,  A: 40, T: 80, C:50 },
-        //     { assessmentName: 'Quiz 4', K: 75,  A: 65, T: 80, C:50 },
-        //     { assessmentName: 'Quiz 5', K: 50,  A: 40, T: 80, C:50 },
-        //     { assessmentName: 'Quiz 6', K: 75,  A: 65, T: 80, C:50 },
-        //     { assessmentName: 'Quiz 7', K: 100, A: 90, T: 80, C:50 },
-        //     { assessmentName: 'Quiz 8', K: 100, A: 90, T: 80, C:50 },
-        //     { assessmentName: 'Quiz 9', K: 100, A: 90, T: 80, C:50 },
-        //     { assessmentName: 'Quiz 10', K: 100, A: 90, T: 80, C:50 },
-        // ],
-        data: grabGrades("c2"), //should be the assessmentTypeId
+        data: [
+            { assessmentName: 'Quiz 1', K: 100, A: 90, T: 80, C:50 },
+            { assessmentName: 'Quiz 2', K: 75,  A: 65, T: 80, C:50 },
+            { assessmentName: 'Quiz 3', K: 50,  A: 40, T: 80, C:50 },
+            { assessmentName: 'Quiz 4', K: 75,  A: 65, T: 80, C:50 },
+            { assessmentName: 'Quiz 5', K: 50,  A: 40, T: 80, C:50 },
+            { assessmentName: 'Quiz 6', K: 75,  A: 65, T: 80, C:50 },
+            { assessmentName: 'Quiz 7', K: 100, A: 90, T: 80, C:50 },
+            { assessmentName: 'Quiz 8', K: 100, A: 90, T: 80, C:50 },
+            { assessmentName: 'Quiz 9', K: 100, A: 90, T: 80, C:50 },
+            { assessmentName: 'Quiz 10', K: 100, A: 90, T: 80, C:50 },
+        ],
+        // data: data,
 
         xkey: 'assessmentName',
         ykeys: ['K', 'A', 'T', 'C'],

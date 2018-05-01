81fbcb9db5cd15d60d8e7084f28ed2074a86b36f
dbzmay <brendonscottmay@gmail.com>
2018-05-01 09:38:36 -0400

    unable to cancel until 7 days after purchase

--- client/includes/cancelSubscription/cancelSubscription.js
+++ client/includes/cancelSubscription/cancelSubscription.js
@@ -2,6 +2,7 @@ import { Template } from 'meteor/templating';
 import { Accounts } from 'meteor/accounts-base';
 
 import '../../main.html';
+import { CurrentDate } from '../../../lib/collections';
 
 Template.cancelSubscription.events({
     'click #cancelSub-yes': function () {
@@ -9,5 +10,25 @@ Template.cancelSubscription.events({
         Meteor.call('cancelSubscriptionBT', customerId, function(){
             Meteor.call('cancelSubscriptionCollection', customerId);
         });
+    }
+});
+
+Template.cancelSubscription.helpers({
+    canCancel: function(){
+        var oneDay = 24*60*60*1000;
+        var dateOfPurchase = Meteor.users.findOne({_id: Meteor.userId()}).subscribed.dateOfPurchase;
+        var currentDate = CurrentDate.findOne({}).date;
+        var diffDays = Math.round(Math.abs((currentDate.getTime() - dateOfPurchase.getTime())/(oneDay)));
+        return diffDays >= 7
+    },
+    cancelationDate: function(){
+        Date.prototype.addDays = function(days) {
+            var dat = new Date(this.valueOf());
+            dat.setDate(dat.getDate() + days);
+            return dat;
+        }
+        var dateOfPurchase = Meteor.users.findOne({_id: Meteor.userId()}).subscribed.dateOfPurchase;
+        var cancelationDate = dateOfPurchase.addDays(7);
+        return cancelationDate.toDateString()
     }
 });
